3447df2a80bf518af68b922f40c1f00734c05c7a
Aaron Powell <me@aaron-powell.com>
2012-03-25 10:59:50 +0000

    initial setup

---
+++ web.js
@@ -0,0 +1,32 @@
+var //pinboard = require('pinboard'),
+    parser = require('sax2json'),
+	request = require('request'),
+	express = require('express'),
+	server = express.createServer();
+
+server.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+ });
+
+server.get('*', function (req, res) {
+    var header = req.headers['authorization'] || '',        // get the header
+	    token = header.split(/\s+/).pop() || '',            // and the encoded auth token
+	    auth = new Buffer(token, 'base64').toString(),    // convert from base64
+	    parts = auth.split(/:/),                          // split on colon
+	    username = parts[0],
+	    password = parts[1];
+
+    var url = "https://" + username + ":" + password + "@api.pinboard.in/v1";
+
+    request({
+        url: url + req.path
+    }, function (error, response, body) {
+        parser.toJson(body, function (x, obj) {
+			res.json(obj);
+		});
+    });
+});
+
+server.listen(process.env.PORT || 1337);
